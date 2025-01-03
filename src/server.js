0030401add3d9d6a08275aa134bd1180a922bee1
anna_vezhdel <avezhdel@gmail.com>
2025-01-03 14:05:02 +0100

    replace

--- src/server.js
+++ src/server.js
@@ -18,8 +18,45 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', getAllContacts);
-  app.get('/contacts/:contactId', getContactById);
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: contacts,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Server error',
+        error: error.message,
+      });
+    }
+  });
+  app.get('/contacts/:contactId', async (req, res) => {
+    const { contactId } = req.params;
+
+    try {
+      const contact = await getContactById(contactId);
+      if (!contact) {
+        return res.status(404).json({
+          status: 404,
+          message: 'Contact not found!',
+        });
+      }
+
+      res.json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contact,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: 'Server error',
+        error: error.message,
+      });
+    }
+  });
 
   app.use((req, res) => {
     res.status(404).json({
