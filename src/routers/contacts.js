74d3ae8a992b6fc0102e542962d6932ef22d7e7d
anna_vezhdel <avezhdel@gmail.com>
2025-01-12 13:30:42 +0100

    rest api

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
 import {
+  addContactController,
+  deleteContactController,
   getContactByIdController,
   getContactsController,
+  patchContactController,
+  upsertContactController,
 } from '../controllers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
@@ -9,5 +13,13 @@ const contactsRouter = Router();
 
 contactsRouter.get('/', ctrlWrapper(getContactsController));
 contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
+
+contactsRouter.post('/', ctrlWrapper(addContactController));
+
+contactsRouter.put('/:contactId', ctrlWrapper(upsertContactController));
+
+contactsRouter.patch('/:contactId', ctrlWrapper(patchContactController));
+
+contactsRouter.delete('/:contactId', ctrlWrapper(deleteContactController));
 
 export default contactsRouter;
