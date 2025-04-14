f80a96c40c65e001ec0f61a4524142fc6d710628
anna_vezhdel <avezhdel@gmail.com>
2025-04-14 19:00:15 +0200

    Merge branch 'main' into hw7-swagger

--- src/services/auth.js
+++ src/services/auth.js
@@ -98,8 +98,6 @@ export const getUser = (filter) => UserCollection.findOne(filter);
 
 export const getSession = (filter) => SessionCollection.findOne(filter);
 
-const template = handlebars.compile(RESET_PASSWORD_TEMPLATE);
-
 export async function requestResetPassword(email) {
   const user = await UserCollection.findOne({ email });
 
